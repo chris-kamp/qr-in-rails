1f4d2d13ebfffc58582ed55832348fe0e69ad4c0
Chris Kamp <chriskamp101@gmail.com>
2021-07-14 14:16:42 +1000

    add presence and unqiueness validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_one :business, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :checkins, dependent: :destroy
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end

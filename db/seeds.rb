baad4a86734813caa9b716d6fe086014d61663bf
Chris Kamp <chriskamp101@gmail.com>
2021-07-15 14:42:39 +1000

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
 User.destroy_all
 Category.destroy_all
 
-user = User.create(
-  email: "foo@bar.com",
+users = User.create(
+  [{email: "user1@user1.com",
   password: "password",
-  username: "foo",
+  username: "user1",
   public: true,
-  bio: "Hi, I'm a user"
+  bio: "Hi, I'm the first user"},
+  {email: "user2@user2.com",
+    password: "password",
+    username: "user2",
+    public: false,
+    bio: "Hi, I'm the second user"}]
 )
 
-bar = Category.create(
-  name: "Bar"
+categories = Category.create([
+ {name: "Bar"},
+ {name: "Restaurant"},
+ {name: "Cafe"}
+] 
 )
 
 address = Address.create(
@@ -18,10 +26,9 @@ address = Address.create(
   postcode: Postcode.create(code: 4000),
   state: State.create(name: "QLD"),
   suburb: Suburb.create(name: "Brisbane City"),
-  business: User.first.create_business(
-    category: bar,
+  business: users[0].create_business(
+    category: categories[0],
     name: 'some business',
-    description: "It's a business",
-    address: address
+    description: "It's a business"
   )
 )

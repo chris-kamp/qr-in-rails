55e7d1d039add0e54b5c3123b5c771e2c3792d1e
Chris Kamp <chriskamp101@gmail.com>
2021-07-15 22:08:36 +1000

    update and fix checkin controller actions

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,33 +1,36 @@
 class CheckinsController < ApplicationController
-    before_action :set_checkin, except: [:index, :create]
-    before_action :authenticate, only: [:create]
+  before_action :set_checkin, only: [:show]
+  before_action :authenticate, only: [:create]
 
-    def index
-        render json: @checkins, include: [:user, :business]
+  def index
+    render status: :not_implemented
+  end
+
+  def show
+    render json: @checkin,
+           include: [
+             { user: { only: :username } },
+             { business: { only: :name } },
+           ]
+  end
+
+  def create
+    @checkin = Checkin.new(checkin_params)
+
+    if @checkin.save
+      render json: @checkin
+    else
+      render json: { errors: @checkin.errors }, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @checkins
-    end
+  private
 
-    def create
-        @checkin = Checkin.new(checkin_params)
+  def set_checkin
+    @checkin = Checkin.find(params[:id])
+  end
 
-        if @checkin.save
-            render json: @checkin
-        else
-            render json: { errors: @checkin.errors }, status: :unprocessable_entity
-        end
-    end
-
-    private
-
-        def set_checkin
-            @checkin = Checkin.find(params[:id])
-        end
-
-        def checkin_params
-            params.permit(:user_id, :business_id, :content)
-        end
-
+  def checkin_params
+    params.permit(:user_id, :business_id)
+  end
 end

f16c2abc4c1155e1fcd86aac0930ee6cec396e9d
Chris Kamp <chriskamp101@gmail.com>
2021-07-15 21:55:36 +1000

    bugfixes in review controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,47 +1,48 @@
 class ReviewsController < ApplicationController
-    before_action :set_review, except: [:index, :create, :update, :destroy]
-    before_action :authenticate, only: [:create, :update, :destroy]
+  before_action :set_review, only: %i[show update destroy]
+  before_action :authenticate, only: %i[create update destroy]
 
-    def index
-        render json: @reviews, include: [:user, :business]
+  def index
+    render status: :not_implemented
+  end
+
+  def show
+    render json: @review,
+           include: [{ user: { only: :username } }, business: { only: :name }]
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      render json: @review, status: :created
+    else
+      render json: { errors: @review.errors }, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @reviews
+  def update
+    if @review.update(review_params)
+      render json: @review
+    else
+      render json: { errors: @review.errors }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @review = Review.new(review_params)
-
-        if @review.save
-            render json: @review, status: :created
-        else
-            render json: { errors: @review.errors }, status: :unprocessable_entity
-        end
+  def destroy
+    if @review.destroy
+      render status: :no_content
+    else
+      render json: { errors: @review.errors }, status: :unprocessable_entity
     end
+  end
 
-    def update
-        if @review.update(review_params)
-            render json: @review
-        else
-            render json: { errors: @review.errors }, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def destroy
-        if @review.destroy
-            render status: :no_content
-        else
-            render json: { errors: @review.errors }, status: :unprocessable_entity
-        end
-    end
+  def set_review
+    @review = Review.find(params[:id])
+  end
 
-    private
-        def set_review
-            @review = Review.find(params[:id])
-        end
-
-        def review_params
-            params.permit(:user_id, :business_id, :content)
-        end 
+  def review_params
+    params.permit(:user_id, :business_id, :content, :rating)
+  end
 end

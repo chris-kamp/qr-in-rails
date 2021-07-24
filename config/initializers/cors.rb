71402fa162d056e33a9a36727ad9764f73140423
Chris Kamp <chriskamp101@gmail.com>
2021-07-24 17:21:44 +1000

    further cors bugfix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env == "development" ? "http://localhost:3000" : 'https://qr-in.netlify.app/'
+    origins Rails.env == "development" ? "http://localhost:3000" : 'https://qr-in.netlify.app'
 
     resource '*',
       headers: :any,

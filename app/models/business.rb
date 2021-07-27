56b051bc56adef803531e6a252758509ab30b256
Anthony <anthonydbeck77@gmail.com>
2021-07-27 12:46:02 +1000

    Commenting on business functions

--- app/models/business.rb
+++ app/models/business.rb
@@ -6,15 +6,20 @@ class Business < ApplicationRecord
   has_many :promotions, dependent: :destroy
   has_one :address, required: true, dependent: :destroy
 
+  # Validation of business attributes
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
 
+  # Filter Businesses where category_id matches given category or categories
   scope :filter_by_category, ->(category_id) { where category_id: category_id }
 
+  # Promotions with the active scope
   def active_promotions
-    self.promotions.active
+    return promotions.active
   end
+
+  # Number of checkins created for this business in the last week
   def weekly_checkin_count
-    self.checkins.where('created_at >= ?', DateTime.now - 7).count
+    return checkins.where('created_at >= ?', DateTime.now - 7).count
   end
 end
